692394a9f5ac8c1fda1345f93e72389542910090
Yusuke Izumi <yizumi@raksul.com>
2017-10-22 14:11:44 +0900

    Dockerize the app

--- Gemfile
+++ Gemfile
@@ -12,10 +12,12 @@ gem 'puma', '~> 3.7'
 gem 'rails', '~> 5.1.4'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
+gem 'ruby_dep'
 
 gem 'coffee-rails', '~> 4.2'
 gem 'jbuilder', '~> 2.5'
 gem 'slim-rails'
+gem 'listen', '>= 3.0.5', '< 3.2'
 
 group :development, :test do
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
@@ -35,7 +37,6 @@ group :test do
 end
 
 group :development do
-  gem 'listen', '>= 3.0.5', '< 3.2'
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'web-console', '>= 3.3.0'
--- config/application.rb
+++ config/application.rb
@@ -11,12 +11,12 @@ module BluegiantUi
     config.load_defaults 5.1
     config.generators do |g|
       g.test_framework :rspec,
-                       fixtures: true,
-                       view_specs: false,
-                       helper_specs: false,
-                       routing_specs: false,
-                       controller_specs: true,
-                       request_specs: false
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: false
       g.fixture_replacement :factory_girl, dir: 'spec/factories'
     end
   end
