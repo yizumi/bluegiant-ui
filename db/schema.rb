08672bf9eb0e0c5eb90766d5c4714935fc8de45c
Jaime Manzone <jaime@JaimenoiMac.local>
2017-10-29 19:40:19 +0900

    Rename Blotter to Market Monitor

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,43 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_171_027_161_509) do
-  create_table 'delayed_jobs', force: :cascade, options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8' do |t|
-    t.integer 'priority', default: 0, null: false
-    t.integer 'attempts', default: 0, null: false
-    t.text 'handler', null: false
-    t.text 'last_error'
-    t.datetime 'run_at'
-    t.datetime 'locked_at'
-    t.datetime 'failed_at'
-    t.string 'locked_by'
-    t.string 'queue'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
-    t.index %w[priority run_at], name: 'delayed_jobs_priority'
+ActiveRecord::Schema.define(version: 20171027161509) do
+
+  create_table "exchanges", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "code", null: false
+    t.string "name"
+    t.decimal "fee", precision: 15, scale: 10
+    t.boolean "trade_enabled"
+    t.boolean "balance_enabled"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["code"], name: "index_exchanges_on_code"
   end
 
-  create_table 'exchanges', force: :cascade, options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8' do |t|
-    t.string 'code', null: false
-    t.string 'name'
-    t.decimal 'fee', precision: 15, scale: 10
-    t.boolean 'trade_enabled'
-    t.boolean 'balance_enabled'
-    t.string 'url'
-    t.boolean 'subscribed', default: false, null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['code'], name: 'index_exchanges_on_code'
-    t.index ['subscribed'], name: 'index_exchanges_on_subscribed'
+  create_table "markets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "exchange_id", null: false
+    t.string "code", null: false
+    t.boolean "subscribed", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["exchange_id", "code"], name: "index_markets_on_exchange_id_and_code"
+    t.index ["subscribed"], name: "index_markets_on_subscribed"
   end
 
-  create_table 'markets', force: :cascade, options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8' do |t|
-    t.integer 'exchange_id', null: false
-    t.string 'code', null: false
-    t.boolean 'subscribed', default: false, null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index %w[exchange_id code], name: 'index_markets_on_exchange_id_and_code'
-    t.index ['subscribed'], name: 'index_markets_on_subscribed'
-  end
 end
